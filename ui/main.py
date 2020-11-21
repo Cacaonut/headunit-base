c461ecf5c2d382dc54f81df170519b27404554a8
Cacaonut <benedikt.kaesen@gmail.com>
2020-11-21 18:03:41 +0100

    update

--- ui/main.py
+++ ui/main.py
@@ -380,7 +380,6 @@ class Ui_MainWindow(object):
         self.current_tab = self.btn_settings
 
     def updateTime(self):
-        os.system("wmctrl -R Headunit")
         now = datetime.now()
         current_time = now.strftime("%H:%M")
         self.label_clock.setText(current_time)
