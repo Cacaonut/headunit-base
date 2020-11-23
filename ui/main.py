ee1017f8265a7f104c0a3338112fdd2b7cd90e0a
Cacaonut <benedikt.kaesen@gmail.com>
2020-11-23 21:07:37 +0100

    update

--- ui/main.py
+++ ui/main.py
@@ -373,9 +373,9 @@ class Ui_MainWindow(object):
         self.btn_android_auto.setEnabled(False)
         self.current_tab.setEnabled(True)
         self.label_title.setText("ANDROID AUTO")
-        self.bindAndroidAuto()
         self.content.setCurrentWidget(self.content_android_auto)
         self.current_tab = self.btn_android_auto
+        self.bindAndroidAuto()
 
     def switchToSettings(self, event):
         self.content.setCurrentWidget(self.content_settings)
