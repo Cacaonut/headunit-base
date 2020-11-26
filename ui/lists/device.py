1e505fffa1ecae6690349baa04f12f3b7d30ffc5
Cacaonut <benedikt.kaesen@gmail.com>
2020-11-26 15:57:49 +0100

    update

--- ui/lists/device.py
+++ ui/lists/device.py
@@ -116,6 +116,8 @@ class Ui_widget(object):
                                      "}")
         self.btn_sound.setObjectName("btn_sound")
         self.btn_sound.mouseReleaseEvent = self.soundClicked
+        if not self.owner.current_tab_connected:
+            self.btn_sound.setVisible(False)
         self.label_btn_sound = QtWidgets.QLabel(self.btn_sound)
         self.label_btn_sound.setGeometry(QtCore.QRect(3, 3, 24, 24))
         self.label_btn_sound.setText("")
