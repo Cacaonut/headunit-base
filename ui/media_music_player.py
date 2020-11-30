389c4014d802af18461c10e5830bead6ab49b06a
Cacaonut <benedikt.kaesen@gmail.com>
2020-11-30 12:00:11 +0100

    update

--- ui/media_music_player.py
+++ ui/media_music_player.py
@@ -274,7 +274,8 @@ class Ui_content(object):
             signal_name='PropertiesChanged',
             dbus_interface='org.freedesktop.DBus.Properties')
 
-        status = self.transport_prop_iface.GetAll("org.bluez.MediaPlayer1")
+        prop = dbus.Interface(obj, "org.freedesktop.DBus.Properties")
+        status = prop.GetAll("org.bluez.MediaPlayer1")
         print(status)
 
     def on_property_changed(self, interface, changed, invalidated):
