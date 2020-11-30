4d1d49105d03b529031e1cdb9c6f07ed2b7e61e5
Cacaonut <benedikt.kaesen@gmail.com>
2020-11-30 14:10:02 +0100

    Update media_music_player.py

--- ui/media_music_player.py
+++ ui/media_music_player.py
@@ -155,70 +155,107 @@ class Ui_content(object):
         self.label_album.setText(_translate("content", ""))
 
     def updateUI(self):
-        if self.slider_pressed or self.fetching_info or self.owner.useBluetooth:
+        if self.slider_pressed or self.fetching_info:
             threading.Timer(0.1, self.updateUI).start()
             return
 
         self.fetching_info = True
 
-        if mixer.music.get_busy():
-            try:
-                id3 = EasyID3(self.owner.current_file)
-                self.label_title.setText(id3["title"][0])
+        if self.owner.useBluetooth:
+            props = self.player_prop_iface.GetAll("org.bluez.MediaPlayer1")
+            if props["Status"] == "playing":
+                self.btn_play.setPixmap(QtGui.QPixmap(":/images/pause.svg"))
+                track = props["Track"]
+                self.label_title.setText(track["Title"])
+                self.label_artist.setText(track["Artist"])
+                self.label_album.setText(track["Album"])
+                
+                length = track["Duration"] / 1000.0
+                length_minutes = int(length / 60)
+                length_seconds = int(length - length_minutes * 60)
+                self.label_length.setText(str(length_minutes) + ":" + f"{length_seconds:02d}")
 
-                try:
-                    self.label_artist.setText(id3["artist"][0])
-                except:
-                    self.label_artist.setText("-")
-                try:
-                    self.label_album.setText(id3["album"][0])
-                except:
-                    self.label_album.setText("-")
+                pos = props["Position"] / 1000.0
+                pos_minutes = int(pos / 60)
+                pos_seconds = int(pos - pos_minutes * 60)
+                self.label_music_pos.setText(str(pos_minutes) + ":" + f"{pos_seconds:02d}")
 
-            except:
-                self.label_title.setText(os.path.basename(self.owner.current_file))
-                self.label_album.setText("-")
-                self.label_artist.setText("-")
+                progress = (pos * 100) / length
+                self.music_slider.blockSignals(True)
+                self.music_slider.setValue(progress)
+                self.music_slider.blockSignals(False)
 
-            try:
-                file = File(self.owner.current_file)
-                artwork = file.tags['APIC:'].data
-                pixmap = QtGui.QPixmap()
-                pixmap.loadFromData(artwork)
-                self.disc_cover.setPixmap(pixmap)
-            except:
+            else:
+                self.btn_play.setPixmap(QtGui.QPixmap(":/images/play.svg"))
+                self.label_title.setText("")
+                self.label_album.setText("")
+                self.label_artist.setText("")
+                self.label_music_pos.setText("0:00")
+                self.label_length.setText("0:00")
                 self.disc_cover.setPixmap(QtGui.QPixmap(":/images/cover.png"))
-
-            self.length_seconds = 0
-            try:
-                mp3 = MP3(self.owner.current_file)
-                self.length_seconds = mp3.info.length
-                length_minutes = int(self.length_seconds / 60)
-                seconds = int(self.length_seconds - length_minutes * 60)
-                self.label_length.setText(str(length_minutes) + ":" + f"{seconds:02d}")
-            except:
-                self.label_length.setText("")
-
-            pos_seconds = self.current_offset + mixer.music.get_pos() / 1000
-            pos_minutes = int(pos_seconds / 60)
-            seconds = int(pos_seconds - pos_minutes * 60)
-            self.label_music_pos.setText(str(pos_minutes) + ":" + f"{seconds:02d}")
-
-            progress = (pos_seconds * 100) / self.length_seconds
-            self.music_slider.blockSignals(True)
-            self.music_slider.setValue(progress)
-            self.music_slider.blockSignals(False)
-
+                self.music_slider.blockSignals(True)
+                self.music_slider.setValue(0)
+                self.music_slider.blockSignals(False)
         else:
-            self.label_title.setText("")
-            self.label_album.setText("")
-            self.label_artist.setText("")
-            self.label_music_pos.setText("0:00")
-            self.label_length.setText("0:00")
-            self.disc_cover.setPixmap(QtGui.QPixmap(":/images/cover.png"))
-            self.music_slider.blockSignals(True)
-            self.music_slider.setValue(0)
-            self.music_slider.blockSignals(False)
+            if mixer.music.get_busy():
+                try:
+                    id3 = EasyID3(self.owner.current_file)
+                    self.label_title.setText(id3["title"][0])
+
+                    try:
+                        self.label_artist.setText(id3["artist"][0])
+                    except:
+                        self.label_artist.setText("-")
+                    try:
+                        self.label_album.setText(id3["album"][0])
+                    except:
+                        self.label_album.setText("-")
+
+                except:
+                    self.label_title.setText(os.path.basename(self.owner.current_file))
+                    self.label_album.setText("-")
+                    self.label_artist.setText("-")
+
+                try:
+                    file = File(self.owner.current_file)
+                    artwork = file.tags['APIC:'].data
+                    pixmap = QtGui.QPixmap()
+                    pixmap.loadFromData(artwork)
+                    self.disc_cover.setPixmap(pixmap)
+                except:
+                    self.disc_cover.setPixmap(QtGui.QPixmap(":/images/cover.png"))
+
+                self.length_seconds = 0
+                try:
+                    mp3 = MP3(self.owner.current_file)
+                    self.length_seconds = mp3.info.length
+                    length_minutes = int(self.length_seconds / 60)
+                    seconds = int(self.length_seconds - length_minutes * 60)
+                    self.label_length.setText(str(length_minutes) + ":" + f"{seconds:02d}")
+                except:
+                    self.label_length.setText("")
+
+                pos_seconds = self.current_offset + mixer.music.get_pos() / 1000
+                pos_minutes = int(pos_seconds / 60)
+                seconds = int(pos_seconds - pos_minutes * 60)
+                self.label_music_pos.setText(str(pos_minutes) + ":" + f"{seconds:02d}")
+
+                progress = (pos_seconds * 100) / self.length_seconds
+                self.music_slider.blockSignals(True)
+                self.music_slider.setValue(progress)
+                self.music_slider.blockSignals(False)
+
+            else:
+                self.btn_play.setPixmap(QtGui.QPixmap(":/images/play.svg"))
+                self.label_title.setText("")
+                self.label_album.setText("")
+                self.label_artist.setText("")
+                self.label_music_pos.setText("0:00")
+                self.label_length.setText("0:00")
+                self.disc_cover.setPixmap(QtGui.QPixmap(":/images/cover.png"))
+                self.music_slider.blockSignals(True)
+                self.music_slider.setValue(0)
+                self.music_slider.blockSignals(False)
 
         self.fetching_info = False
         threading.Timer(0.1, self.updateUI).start()
@@ -271,15 +308,6 @@ class Ui_content(object):
             sys.exit('Error: Media Player not found.')
         if not self.transport_prop_iface:
             sys.exit('Error: DBus.Properties iface not found.')
-
-        bus.add_signal_receiver(
-            self.on_property_changed,
-            bus_name='org.bluez',
-            signal_name='PropertiesChanged',
-            dbus_interface='org.freedesktop.DBus.Properties')
-
-        props = self.player_prop_iface.GetAll("org.bluez.MediaPlayer1")
-        print(props)
 
     def on_property_changed(self, interface, changed, invalidated):
         if interface != 'org.bluez.MediaPlayer1':
