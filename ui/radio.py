73c9a4ab468c68e061abcb73d7ef0fb74dfd5f7a
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-07 14:54:55 +0100

    Update radio.py

--- ui/radio.py
+++ ui/radio.py
@@ -420,7 +420,7 @@ class Ui_content(object):
         self.changeFrequency(self.current_freq - 0.1)
 
     def fetchRDSOutput(self):
-        command = 'rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 20 -F 9 -f 105.7M | readsea --feed-through | aplay -r ' \
+        command = 'rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 20 -F 9 -f 105.7M | redsea --feed-through | aplay -r ' \
                   '171000 -f S16_LE'
         self.process = subprocess.Popen(command, stderr=subprocess.PIPE, shell=True)
         while True:
