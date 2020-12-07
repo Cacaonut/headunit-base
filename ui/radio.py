afd4b919ce6aad8b6a079f8f679b7bb0a7815d69
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-07 12:47:25 +0100

    Update radio.py

--- ui/radio.py
+++ ui/radio.py
@@ -400,9 +400,8 @@ class Ui_content(object):
 
     def play(self):
         self.playing = True
-        command = shlex.split('rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 20 -F 9 -f 105.7M '
-                                                    '| redsea --feed-through '
-                                                    '| aplay -r 171000 -f S16_LE')
+        command = 'rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 20 -F 9 -f 105.7M | reads --feed-through | aplay -r ' \
+                  '171000 -f S16_LE '
         print(command)
         self.process = subprocess.Popen(command, stdout=subprocess.PIPE)
         # self.fetchRDSOutput()
