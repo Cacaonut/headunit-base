70337415dc60e02e0f361be23f40f706b67a656c
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-23 14:44:04 +0100

    Update main.py

--- ui/main.py
+++ ui/main.py
@@ -272,7 +272,7 @@ class Ui_MainWindow(object):
         self.ui_media = media.Ui_content()
         self.ui_media.setupUi(self.content_media)
         if self.ui_media.useBluetooth:
-            self.ui_media.switchToBluetooth()
+            self.ui_media.switchToBluetooth(None)
         self.content.addWidget(self.content_media)
 
         # Radio
