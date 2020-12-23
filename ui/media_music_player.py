bd0f6e9f2618c9fcb428fe5eb73247478233bd0d
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-23 19:20:10 +0100

    Update media_music_player.py

--- ui/media_music_player.py
+++ ui/media_music_player.py
@@ -163,6 +163,7 @@ class Ui_content(object):
 
         if self.owner.useBluetooth:
             if not hasattr(self, "player_prop_iface"):
+                print("no interface found")
                 threading.Timer(0.1, self.updateUI).start()
                 return
 
