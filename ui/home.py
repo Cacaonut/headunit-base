32978e6b0c840b9a2dc627d0a6259287b4f86ab6
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-23 14:59:40 +0100

    Update home.py

--- ui/home.py
+++ ui/home.py
@@ -145,7 +145,6 @@ class Ui_content(object):
         self.btn_play_media.setPixmap(QtGui.QPixmap(":/images/play.svg"))
         self.btn_play_media.setScaledContents(True)
         self.btn_play_media.setObjectName("btn_play_media")
-        self.btn_play_media.mouseReleaseEvent = self.owner.ui_media.ui_music_player.playBtnPressed
         self.label_song = QtWidgets.QLabel(self.widget_media)
         self.label_song.setGeometry(QtCore.QRect(20, 70, 170, 30))
         font = QtGui.QFont()
@@ -240,6 +239,7 @@ class Ui_content(object):
 
             # Media
             if hasattr(self.owner, "ui_media"):
+                self.btn_play_media.mouseReleaseEvent = self.owner.ui_media.ui_music_player.playBtnPressed
                 self.label_song.setText(self.owner.ui_media.ui_music_player.label_title.text())
                 playing = True
                 print("Using BT: " + str(self.owner.ui_media.useBluetooth))
