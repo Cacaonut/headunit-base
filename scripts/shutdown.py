5d1ad4c4fa4c6347aefc7a29c5c9002c3eac61a1
Cacaonut <benedikt.kaesen@gmail.com>
2020-12-24 15:45:48 +0100

    Update shutdown.py

--- scripts/shutdown.py
+++ scripts/shutdown.py
@@ -1,8 +1,13 @@
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+    GPIO.wait_for_edge(3, GPIO.FALLING)
+    time.sleep(10)
+    if not GPIO.input(3):
+        subprocess.call(['shutdown', '-h', 'now'], shell=False)
+
